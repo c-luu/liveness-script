be211e63a0a52ff480ead24f3b22be6f79953fda
cluu <cluu30@gatech.edu>
2018-11-03 18:23:34 -0500

    finished unit test

--- TestLiveness.py
+++ TestLiveness.py
@@ -9,20 +9,20 @@ class TestLiveness(unittest.TestCase):
         # import pdb; pdb.set_trace()
 
         # arrange
-        succs = dict([ (0, { 0, 1, 2}) ])
+        succs = dict([ (0, { 0, 1}) ])
         defs = []
         uses = []
-        live_in = dict([ (0, {0})
-                          , (1, {1})
-                          , (2, set()) ])
+        live_in = dict([ (0, { 0 })
+                          , (1, { 1, 2 }) ])
         live_out = []
+
         l = liveness.Liveness(succs, defs, uses, live_in, live_out)
 
         # act
         result = l.out_set(0) 
 
         # assert
-        self.assertEqual(result, { 0: set() })
+        self.assertEqual(result, { 0: { 0, 1, 2 }})
 
 if __name__ == '__main__':
         unittest.main()
