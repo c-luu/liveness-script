f4fdff19e1493677dc32bed94ac921fbb3085b49
cluu <cluu30@gatech.edu>
2018-11-03 19:04:32 -0500

    added test file

--- Liveness.py
+++ Liveness.py
@@ -1,14 +1,4 @@
-# S := dict (I, { successors } )
-# D := dict (I, { defs } )
-# U := dict (I, { uses } )
-
-# I := dict (I, { in } )
-# O := dict (I, { out } )
-
-# step 0: build succs dict
-# step 1: build def and use dicts
-# step 2: build out dict
-# step 3: build in dict
+import csv
 
 class Liveness():
     def __init__(self, succs, defs, uses, live_in, live_out):
